1df776aa2ce53297624f8ee00e44d894029a3e43
rafaelsgomes <rafael_souza.41@hotmail.com>
2020-08-25 11:48:23 -0300

    Launchstore: Starting Photos Upload

--- Launchstore/public/JS/script.js
+++ Launchstore/public/JS/script.js
@@ -1,59 +1,102 @@
-const currentPage = location.pathname
-const menuItems = document.querySelectorAll('header .links a')
+const Mask = {
+    apply(input, func){
+        setTimeout(()=>{
+            input.value = Mask[func](input.value)
+        }, 1)
+    },
+    formatBRL(value){
+        value = value.replace(/\D/g, "")
 
-for(item of menuItems){
-    if(currentPage.includes(item.getAttribute('href'))){
-        item.classList.add('active')
+        return value = new Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL'
+        }).format(value/100)
     }
 }
 
-//Paginate
-function paginate(selectedPage, totalPages){
-    let pages = [], oldpage
+const PhotosUpload = {
+    input: "",
+    preview: document.querySelector('#photos-preview'),
+    ulploadLimit: 6,
+    files: [],
+    handleFileInput(event){
+        const { files: fileList } = event.target
 
-    for(let currentPage = 1; currentPage <= totalPages; currentPage++){ 
-        const firstAndLastPage = currentPage == 1 || currentPage == totalPages
-        const pageAfterSelectedPage = currentPage <= selectedPage + 2
-        const pageBeforeSelectedPage = currentPage >= selectedPage - 2
+        PhotosUpload.input = event.target
 
-        if(firstAndLastPage || pageBeforeSelectedPage && pageAfterSelectedPage){
-            if(oldpage && currentPage - oldpage > 2){
-                pages.push('...')
+        if(PhotosUpload.hasLimit(event)) return
+
+        Array.from(fileList).forEach(file => {
+            PhotosUpload.files.push(file)
+            
+            const reader = new FileReader()
+
+            reader.onload = ()=>{
+                const image = new Image()
+                image.src = String(reader.result)
+
+                const div = PhotosUpload.getContainer(image)
+                PhotosUpload.preview.appendChild(div)
             }
-            if(oldpage && currentPage - oldpage == 2){
-                pages.push(oldpage + 1)
-            }
-            pages.push(currentPage)
-            oldpage = currentPage
-    }
-    }
-    return pages
-}
+            reader.readAsDataURL(file)
+        } )
 
-function createPagination(pagination){
-    
-    const filter = pagination.dataset.filter
-    const page = +pagination.dataset.page
-    const totalPages = +pagination.dataset.total
-    const pages = paginate(page, totalPages)
-    let paginationElements = ''
+        PhotosUpload.input.files = PhotosUpload.getAllFiles()
+    },
+    hasLimit(event){
+        const { ulploadLimit, input, preview } = PhotosUpload
+        const { files: fileList } = input
 
-    for (let page of pages){
-        if(String(page).includes("...")){
-            paginationElements += `<span>${page}</span>`
-        }else{
-            if(filter){
-                paginationElements += `<a href="?page=${page}&filter=${filter}">${page}</a>`
-            }else{
-                paginationElements += `<a href="?page=${page}">${page}</a>`
-            } 
+        if(fileList.length > ulploadLimit){
+            alert(`Envie no máximo ${ulploadLimit} fotos`)
+            event.preventDefault()
+            return true
         }
+
+        const photosDiv = []
+        preview.childNodes.forEach(item => {
+            if (item.classList && item.classList.value == 'photo')
+                photosDiv.push(item)
+        })
+        const totalPhotos = fileList.length + photosDiv.length
+        if(totalPhotos > ulploadLimit){
+            alert("Você atingiu o limite máximo de fotos")
+            event.preventDefault()
+            return true
+        }
+        return false
+    },
+    getAllFiles(){
+        const dataTransfer = new ClipboardEvent("").clipboardData || new DataTransfer()
+
+        PhotosUpload.files.forEach(file = dataTransfer.items.add(file))
+    
+        return dataTransfer.files
+    },
+    getContainer(image){
+        const div = document.createElement('div')
+            div.classList.add('photo')
+            div.onclick = PhotosUpload.removePhoto
+            div.appendChild(image)
+            div.appendChild(PhotosUpload.getRemoveButton())
+
+            return div
+    },
+    getRemoveButton(){
+        const button = document.createElement('i')
+        button.classList.add('material-icons')
+        button.innerHTML = "close"
+        return button
+    },
+    removePhoto(event){
+        const photoDiv = event.target.parentNode
+        const photosArray = Array.from(PhotosUpload.preview.children)
+        const index = photosArray.indexOf(photoDiv)
+
+        PhotosUpload.files.splice(index, 1)
+        PhotosUpload.input.files = PhotosUpload.getAllFiles()
+
+        photoDiv.remove()
+        
     }
-
-    pagination.innerHTML = paginationElements
-}
-const pagination = document.querySelector(".pagination")
-
-if(pagination){
-    createPagination(pagination)
 }
