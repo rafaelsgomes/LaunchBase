c0e547883f479e7552cdba6b3b0a3acf5bb668f8
rafaelsgomes <rafael_souza.41@hotmail.com>
2020-09-01 00:15:15 -0300

    Launchstore: SearchController

--- Launchstore/src/routes.js
+++ Launchstore/src/routes.js
@@ -3,9 +3,15 @@ const routes = express.Router()
 const multer =  require('./app/middlewares/multer')
 const ProductController = require('./app/controllers/ProductController')
 const HomeController = require('./app/controllers/HomeController')
+const SearchController = require('./app/controllers/SearchController')
 
+//Home
 routes.get('/', HomeController.index)
 
+//Search
+routes.get("/products/search", SearchController.index)
+
+//Products
 routes.get('/products/create', ProductController.create)
 routes.get('/products/:id', ProductController.show)
 routes.get('/products/:id/edit', ProductController.edit)
@@ -17,6 +23,5 @@ routes.delete('/products', ProductController.delete)
 routes.get('/ads/create', (req, res)=>{
     return res.redirect('/products/create')
 })
-
 
 module.exports = routes
