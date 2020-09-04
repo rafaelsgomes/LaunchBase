fe192ba02d92e8e6243ebae22bba8f2ddef18da5
rafaelsgomes <rafael_souza.41@hotmail.com>
2020-09-03 21:21:24 -0300

    Launchstore: Start User Registration

---
+++ Launchstore-User-Registration/src/routes/users.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const routes = express.Router()
+
+const SessionController = require('../app/controllers/SessionController')
+const UserController = require('../app/controllers/UserController')
+
+// Users
+    // LOGIN
+//     routes.get('/login', SessionController.loginForm)
+//     routes.post('/login', SessionController.login)
+    // LOGOUT
+//     routes.post('/logout', SessionController.logout)
+    // PASSWORD - RESET / FORGOT
+//     routes.get('/forgot-password', SessionController.forgotForm)
+//     routes.get('/password-reset', SessionController.resetForm)
+//     routes.post('/forgot-password', SessionController.forgot)
+//     routes.post('/password-reset', SessionController.reset)
+    // USER REGISTER - UserController
+    routes.get('/register', UserController.registerForm)
+    // routes.post('/register', UserController.post)
+    
+    // routes.get('/', UserController.show)
+    // routes.put('/', UserController.update)
+    // routes.delete('/', UserController.delete)
+
+    module.exports = routes
